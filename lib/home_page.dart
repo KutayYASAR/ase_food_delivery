c2b1a1194405d8eda0cc19be08daa19c50002d7f
Kutay Yaşar <47702147+KutayYASAR@users.noreply.github.com>
2021-08-07 19:51:43 +0300

    basic design finish

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -35,41 +35,75 @@ class HomePage extends StatelessWidget {
               children: [
                 Padding(
                   padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
-                  child: Text('Kampanyalı Restoranlar',style: TextStyle(fontWeight: FontWeight.bold),),
+                  child: Text('Kampanyalı Restoranlar',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),),
                 ),
                 TextButton(onPressed: (){print(restaurantData);} , child: Text('Tümünü Gör',style: TextStyle(color: Colors.orange),))
               ],
             ),
             Flexible(
               flex: 4,
-              child: SizedBox(
-                height: 128,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (BuildContext context, int index) {
-                  return horizontalListCard(restaurantData[index][0],restaurantData[index][1],restaurantData[index][2],restaurantData[index][3]);
-                 },
-                 itemCount: restaurantData.length,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                child: SizedBox(
+                  height: 128,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (BuildContext context, int index) {
+                    return horizontalListCard(restaurantData[index][0],restaurantData[index][1],restaurantData[index][2],restaurantData[index][3]);
+                   },
+                   itemCount: restaurantData.length,
+                  ),
                 ),
               ),
             ),
                 Align(alignment: Alignment.centerLeft, child: Padding(
                   padding: const EdgeInsets.fromLTRB(8, 0, 0, 8),
-                  child: Text('Mutfaklar',style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.left,),
+                  child: Text('Mutfaklar',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16)),
                 )),
             Flexible(
               flex: 3,
-              child: SizedBox(
-                height: 85,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (BuildContext context, int index) {
-                  return horizontalListCategory(categoryData[index][0],categoryData[index][1]);
-                 },
-                 itemCount: categoryData.length,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                child: SizedBox(
+                  height: 85,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (BuildContext context, int index) {
+                    return horizontalListCategory(categoryData[index][0],categoryData[index][1]);
+                   },
+                   itemCount: categoryData.length,
+                  ),
                 ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Align(alignment: Alignment.centerLeft, child: Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 0, 0, 8),
+                        child: Text('Restoranlar',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),textAlign: TextAlign.center,),
+                      )),
+                    TextButton.icon(onPressed: (){}, icon: Icon(Icons.format_list_bulleted,size: 16,), label: Text('Filtrele'),style: ButtonStyle(foregroundColor: MaterialStateProperty.all(Colors.orange)),)
+                ],
+              ),
+            ),
+            Divider(
+              thickness: 1,
+              height: 1,
+            ),
+             Flexible(
+              flex: 15,
+              child: ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                return verticalListCardBig(restaurantData[index][0],restaurantData[index][1],restaurantData[index][2],restaurantData[index][3]);
+               },
+               itemCount: restaurantData.length
               ),
             ),
           ],
@@ -140,6 +174,59 @@ class HomePage extends StatelessWidget {
                      
                     ],
                   ),
+                  ),
+                ],
+              );
+  }
+  Wrap verticalListCardBig(String restaurantName,String description,String imageURL,String rating) {
+    return Wrap(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                      children: [
+                           
+                        Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Container(
+                              child: Image(image: NetworkImage(imageURL),
+                              fit: BoxFit.cover,
+                              ),
+                              height: 200,
+                              width: 5000,
+                            ),
+                            Positioned(
+                              bottom: 8,
+                              left: 8,
+                              child: Card(child: Row(children: [Icon(Icons.star_rate, size: 14,color: Colors.white,),Padding(
+                                padding: const EdgeInsets.fromLTRB(4, 0, 6, 0),
+                                child: Text(rating,style:TextStyle(fontWeight: FontWeight.w500,color: Colors.white),),
+                              )],), color: Colors.greenAccent[400],),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          
+                             child: Padding(
+                               padding: const EdgeInsets.fromLTRB(12, 0, 0, 0),
+                               child: ListTile(
+                                 dense: true,
+                                 visualDensity: VisualDensity(horizontal: 0, vertical: -3),
+                                 contentPadding: EdgeInsets.symmetric(horizontal: 4,vertical: 0 ),
+                                 leading: Icon(Icons.motorcycle_outlined,size: 24,),
+                                  title: Text(restaurantName,style: TextStyle(fontSize: 14, fontWeight: FontWeight.w800 
+                                  ),),
+                                  subtitle: Text(description, style: TextStyle(fontSize: 12),),),
+                             ),
+                           ),
+                      ],
+                    ),
+                    ),
                   ),
                 ],
               );
