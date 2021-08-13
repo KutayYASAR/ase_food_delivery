232a97824b8624d760dc580454394a18b620907a
SemihYlmaz <56646018+SemihYlmaz@users.noreply.github.com>
2021-08-13 12:00:56 +0300

    Order Evaluation Page Created

---
+++ lib/order_evaluation_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class OrderEvaluation extends StatefulWidget {
+  const OrderEvaluation({ Key? key }) : super(key: key);
+
+  @override
+  _OrderEvaluationState createState() => _OrderEvaluationState();
+}
+
+class _OrderEvaluationState extends State<OrderEvaluation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
